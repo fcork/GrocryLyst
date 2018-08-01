be408c8b26f3ecd0919bbb493b29b4c8e9768231
frank <frankcorkery@gmail.com>
2018-08-01 13:55:24 -0400

    altered database functions to keep heroku database responding

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -7,61 +7,88 @@ var dbURL = process.env.CLEARDB_DATABASE_URL || {
 }
 
 
-var connection = mysql.createConnection(dbURL);
+var connectionPool = mysql.createPool(dbURL);
 
-connection.connect();
+// connection.connect();
 
 var getGroceryList = function(list_id, callback) {
   let queryString = `SELECT * FROM groceryItems where list_id=${list_id}`
-  connection.query(queryString, function(err, results, fields) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
+  connectionPool.getConnection((err, connection) => {
+    if (err) console.log(err)
+    else {
+      connection.query(queryString, function(err, results, fields) {
+        connection.release()
+        if(err) {
+          callback(err, null);
+        } else {
+          callback(null, results);
+        }
+      });
     }
-  });
+  })
+  
 };
 
 var addGroceryItem = function(item, list_id, callback) {
   let queryString = `INSERT INTO groceryItems VALUES (default, '${item}', ${list_id})`
-  connection.query(queryString, function(err, results) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, results)
+  connectionPool.getConnection((err, connection) => {
+    if (err) console.log(err)
+    else {
+      connection.query(queryString, function(err, results) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, results)
+        }
+      })
     }
   })
 }
 
 var deleteGroceryItem = function(item, callback) {
   let queryString = `DELETE FROM groceryItems WHERE food='${ item }'`
-  connection.query(queryString, function(err, results) {
-    if (err) {
-      callback (err, null)
-    } else {
-      callback(null, results)
+  connectionPool.getConnection((err, connection) => {
+    if (err) console.log(err)
+    else {
+      connection.query(queryString, function(err, results) {
+        if (err) {
+          callback (err, null)
+        } else {
+          callback(null, results)
+        }
+      })
     }
   })
 }
 
 var addGroceryList = function(list, callback) {
   let queryString = `INSERT INTO groceryLists VALUES (default, '${ list }')`
-  connection.query(queryString, (err, results) => {
-    if (err) {
-      callback(err, null)
-    } else {
-      callback(null, results)
+  connectionPool.getConnection((err, connection) => {
+    if (err) console.log(err)
+    else {
+      connection.query(queryString, (err, results) => {
+        if (err) {
+          callback(err, null)
+        } else {
+          callback(null, results)
+        }
+      })
     }
   })
 }
 
 var getLists = function(callback) {
   let queryString = `SELECT * FROM groceryLists`
-  connection.query(queryString, (err, results) => {
-    if (err) {
-      callback(err, null)
-    } else {
-      callback(null, results)
+  connectionPool.getConnection((err, connection) => {
+    if (err) console.log(err)
+    else {
+      connection.query(queryString, (err, results) => {
+        if (err) {
+          callback(err, null)
+        } else {
+          callback(null, results)
+        }
+      })
     }
   })
 }
