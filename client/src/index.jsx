d24fc2184ef734b6e353efabcc4e8c72ce5f445c
frank <frankcorkery@gmail.com>
2018-09-26 14:47:06 -0400

    code cleanup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,14 +9,10 @@ import Welcome from './components/Welcome.jsx';
 import firebase from '../../firebase/firebase.js';
 import { RingLoader } from 'react-spinners';
 
-// import socketIOClient from 'socket.io-client'
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      groceryList: ['apples, bananas'], 
-      signedUp: false,
+    this.state = {  
       googleUserData: null,
       userStats: null,
       loading: true
@@ -27,13 +23,11 @@ class App extends React.Component {
     this.googleSignOut = this.googleSignOut.bind(this);
     this.handleSignUp = this.handleSignUp.bind(this);
     this.mapDBUserDataToState = this.mapDBUserDataToState.bind(this);
-    // this.addGrocery = this.addGrocery.bind(this)
-    // this.getGroceries = this.getGroceries.bind(this)
+
   }
 
   componentDidMount() {
     this.authListener();
-    // this.mapDBUserDataToState();
     console.log(this.state.googleUserData)
     console.log(this.state.userStats)
   }
@@ -45,7 +39,6 @@ class App extends React.Component {
           googleUserData: Object.assign( {}, user.providerData[0] ),
           loading: false
         }, this.mapDBUserDataToState);
-        // this.mapDBUserDataToState();
         console.log(user.providerData[0]);
       } else {
         this.setState({
@@ -63,7 +56,6 @@ class App extends React.Component {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect( provider )
       .then( () => {
-        // this.mapDBUserDataToState();
         return;
       })
       .catch( (err) => {
@@ -100,7 +92,6 @@ class App extends React.Component {
   handleSignUp (username) {
     axios.post('/user', {params: {email: this.state.googleUserData.email, fullName: this.state.googleUserData.displayName, username: username}})
       .then(() => {
-        // this.setState({signedUp: true})
         this.mapDBUserDataToState();
         return 
       })
@@ -110,43 +101,13 @@ class App extends React.Component {
 
   }
 
-
-  // componentDidMount() {
-  //   // this.getGroceries();
-  // }
-
-  // send() {
-  //   const socket = socketIOClient(this.state.endpoint);
-  //   socket.emit('client action', this.state.color)
-  // }
-
-  // getGroceries() {
-  //   axios.get('/list')
-  //     .then((response) => {
-  //       console.log(response.data)
-  //       this.setState({groceryList: response})
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     })
-  // }
-
-  // addGrocery(e) {
-  //   e.preventDefault();
-  //   console.log(e.target.value)
-  //   axios.post('/list', {params : {item: e.target.value}})
-  //     .then((response) => {
-  //       this.setState({groceryList: [...groceryList, e.target.value]})
-  //     })
-  // }
-
   render () {
     if ( this.state.loading ) {
       return (
         <div className="loading-spinner">
           <RingLoader
             sizeUnit={'px'}
-            size={200}
+            size={400}
             color={'black'}
             loading={ this.state.loading }
           />
